dd612bd08371e9a3899a7026a92b4cfb3c4a3520
cmdmac <cmdmac@126.com>
2024-07-22 21:57:17 +0800

    add two sockets transfrer

--- routes/ws.js
+++ routes/ws.js
@@ -3,43 +3,43 @@ const route = express.Router() // 实例化路由对象
 
 // const getNowTime = require('./utils/index')
 
-// demo
-route.get('/demo', (req, res) => {
-  let apiRes = {
-    code: 0,
-    msg: "成功",
-    data: '请求通过'
-  }
-  res.send(apiRes)
-})
 
-/**
- * route.ws('/url',(ws, req)=>{  })
- * 建立WebSocket服务，并指定对应接口url，及相应回调
- * ws为实例化的对象，req即为请求
- * 
- * ws.send方法用来向客户端发送信息
- * ws.on方法用于监听事件（如监听message事件，或监听close事件）
- * */
-route.ws('/ws', (ws, req) => {
-  // console.log('连接成功', ws)
+var wsWeb = null;
+var wsMobile = null;
 
-  ws.send('来自服务端推送的消息')
+//connect with browser
+route.ws('/web', (ws, req) => {
+
+  // console.log("ws from web req=" + JSON.stringify(req));
+  wsWeb = ws;
 
   ws.on('message', function (msg) {
-    ws.send(`收到客户端的消息为：${msg}，再返回去`)
+    console.log(msg);
+    // console.log(msg.command);
+    // ws.send(msg);
+    var obj = JSON.parse(msg);
+    console.log(obj.command);
+
+    if (obj) {
+      // console.log('parse and send')
+      if (wsMobile) {
+        wsMobile.send(obj);
+        console.log('trasfer msg to mobile msg=' + msg);
+      }
+    }
   })
 
-  // 使用定时器不停的向客户端推动消息
-  let timer = setInterval(() => {
-    ws.send(`服务端定时推送消息: ---------`)
-  }, 1000)
-
+  
   ws.on('close', function (e) {
-    // console.log('连接关闭')
-    clearInterval(timer)
-    timer = null
+      wsWeb = null;
   })
 })
 
-module.exports = route // 暴露出去方便管理
+
+// connect with esp32
+route.ws('/mobile', (ws, req) => {
+  wsMobile = ws;
+
+})
+
+module.exports = route 
