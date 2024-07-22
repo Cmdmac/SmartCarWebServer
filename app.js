0183a44d38473ab731427f0cfad7c7125cbc68a0
cmdmac <cmdmac@126.com>
2024-07-22 20:09:42 +0800

    add websocket

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const expressWs = require('express-ws')(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +23,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// init websocket
+const Router = require('./routes/ws');
+app.use('/', Router)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,11 +44,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-
-const expressWs = require('express-ws')(app);
-var wsRouter = require('./routes/index');
-
-
-app.use('/ws', wsRouter)
+// app.listen(10000, (req, res) => {
+// 	console.log('listen')
+// })
 
 module.exports = app;
