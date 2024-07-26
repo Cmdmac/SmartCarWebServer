42936db580a4990936b843d4679fba409201ccc3
cmdmac <cmdmac@126.com>
2024-07-26 21:20:02 +0800

    convert svg map to json map format

--- svg2json.js
+++ svg2json.js
@@ -1,7 +1,7 @@
 const xml2js = require('xml2js');
 const fs = require('fs');
 
-fs.readFile('xhy.svg', (err, data) => {
+fs.readFile('xhy2.svg', (err, data) => {
     if (err) throw err;
 
     const parser = new xml2js.Parser({
@@ -16,11 +16,12 @@ fs.readFile('xhy.svg', (err, data) => {
         // console.log(result.svg.g.g.path);
 
         let map = {Floors: [], building: {_id: 0, _xLon: 222, Tel: "23", Address: "dasf", Brief: "sdf", Area: 2342.23, FloorsId: "-1, 1,2,3,4,5", 
-            Name_en: "", Version: 5, High: 1, FrontAngle: 0.232234, Mall: 1, Time: "", UnderFloors: 1, Type: 6, Adcode: 110105, DefaultFloor: 1, Remark: "sdfdf", _yLat: 22.234, Outline: [], Name: "sdfs", GroudFloors: 6}};
-        let floor = {Brief: "sdsd", ImageLayer: {Area: 2234.02}, PubPoint: {}, FuncArea: {}, Name_en: "ssdf", Outline: [], _id: -1, High: 5, Name: "B1"};
+            Name_en: "", Version: 5, High: 1, FrontAngle: 0.232234, Mall: 1, Time: "", UnderFloors: 1, Type: 6, Adcode: 110105, DefaultFloor: 0, Remark: "sdfdf", _yLat: 22.234, Outline: [], Name: "sdfs", GroudFloors: 6}};
+        let floor = {Brief: "sdsd", ImageLayer: {Area: 2234.02}, PubPoint: [], FuncAreas: [], Name_en: "ssdf", Outline: [[[]]], _id: 0, High: 5, Name: "B1"};
         map.Floors[0] = floor;
         for (var i = 0; i < result.svg.g.g.path.length; i++) {
             var p = result.svg.g.g.path[i].$;
+            // console.log(p);
             if (p['fill-rule']) {
                 // 矩形区域，获取4个坐标
                 /*
@@ -73,20 +74,35 @@ fs.readFile('xhy.svg', (err, data) => {
                   }
                 */
                 let points = p.d.replaceAll('M', '').replaceAll('L', '').replaceAll('C', '').replaceAll('Z', '').replaceAll(/\s+/g,' ').trim().split(' ').map(item => parseFloat(item));
-                let shop = {_id: Date.now(), Name: "block", name: "block", Name_en: "block", Area: 0, BrandShop: 70000, Brief: "", Category2: 102, ShoppNo: "", Brand: 1000, Type: 6001, Outline: points, dianping_id: 0};
-                shop.Area = Math.abs(points[1] - points[3]) * Math.abs(points[0] - points[7]);
-                console.log(points);
-                console.log(Math.abs(points[0] - points[3]) * Math.abs(points[1] - points[7]));
-                // console.log(shop);
+                let shop = {_id: Date.now(), Name: "block", name: "block", Name_en: "block", Center: [], Area: 0, BrandShop: 70000, Brief: "", Category2: 102, ShoppNo: "", Brand: 1000, Type: 6001, Outline: [[points]], dianping_id: 0};
+                let w = Math.abs(points[0] - points[4]);
+                let h = Math.abs(points[1] - points[5]);
+                shop.Center.push((points[0] + points[4]) / 2);
+                shop.Center.push((points[1] + points[5]) / 2);
+                shop.Area = w * h;
+                // shop.Area = Math.abs(points[1] - points[3]) * Math.abs(points[0] - points[7]);
+                // console.log(points);
+                // console.log(Math.abs(points[0] - points[3]) * Math.abs(points[1] - points[7]));
+                console.log(shop.Center);
                 // calculate area
-            } else if (p.fill == "none" && p['fill-opacity'] != 1) {
+                floor.FuncAreas.push(shop);
+            } else if (p.fill == "none" && p['fill-opacity'] != 1 && p.stroke == "rgb(0,0,0)" && p['stroke-opacity'] != 1) {
                 // console.log(p)
                 let points = p.d.replaceAll('M', '').replaceAll('L', '').replaceAll('C', '').replaceAll('Z', '').replaceAll(/\s+/g,' ').trim().split(' ');
                 // console.log(points);
-                points.forEach(item =>  floor.Outline.push(parseFloat(item)));
+                points.forEach(item =>  floor.Outline[0][0].push(parseFloat(item)));
             }
         }
-        map.building.Outline = floor.Outline;
+        map.building.Outline = floor.Outline[0][0];
+        let data = {data: map};
         // console.log(map);
+        console.log(floor.Outline[0][0].length)
+        let c = JSON.stringify(data);
+
+        fs.writeFile('indoorMap.json', c, err => {
+            if (err) {
+                console.error(err);
+            }
+        })
     });
 });
