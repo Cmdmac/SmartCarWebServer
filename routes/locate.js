48ee8013ef81b012e22232ade4243e9a40b51bb5
cmdmac <cmdmac@126.com>
2024-07-31 10:55:41 +0800

    add doubao tts config

--- routes/locate.js
+++ routes/locate.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var locateByRSSI = require('../locateByRSSI');
+var locateByRSSI = require('../lib/locateByRSSI');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
