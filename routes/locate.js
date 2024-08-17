2d57fb366e5e29240be949ba30dda22521c7976b
cmdmac <cmdmac@126.com>
2024-08-17 19:13:54 +0800

    fix get beacon error

--- routes/locate.js
+++ routes/locate.js
@@ -7,9 +7,10 @@ const {PathFinding} = require("astarjs");
 const beacons = new Map();
 loadBeacons('./indoorMap.json').then((data) => {
     data.forEach(beacon => {
+        console.log('load beacon ' + beacon.mac + " x=" + beacon.x + ",y=" + beacon.y);
         beacons.set(beacon.mac, beacon);
     })
-    console.log('loaded beacons');
+    
 }).catch((err) => {
     console.log(err);
 });
@@ -19,12 +20,14 @@ router.get('/', function(req, res, next) {
     const mac1 = req.query.mac1;
     const mac2 = req.query.mac2;
     const mac3 = req.query.mac3;
+
+    // console.log('mac1=' + mac1 + ',mac2=' + mac2 + ',mac3=' + mac3);
     const beacon1 = beacons.get(mac1);
     const beacon2 = beacons.get(mac2);
-    const beacon3 = beacons.get(mac2);
-    // console.log(beacon1);
-    // console.log(beacon2);
-    // console.log(beacon3);
+    const beacon3 = beacons.get(mac3);
+    console.log(beacon1);
+    console.log(beacon2);
+    console.log(beacon3);
     let location = locateByRSSI(beacon1, beacon2, beacon3, req.query.rssi1, req.query.rssi2, req.query.rssi3);
     // console.log(location);
     if (location) {
