892d7924399954322d4e4f61e617442be11f7d88
cmdmac <cmdmac@126.com>
2024-09-01 15:25:08 +0800

    adjust websockets

--- routes/ws.js
+++ routes/ws.js
@@ -3,46 +3,38 @@ const route = express.Router() // 实例化路由对象
 
 // const getNowTime = require('./utils/index')
 
-
-var wsWeb = null;
-var wsMobile = null;
-
 const httpwsbridge = require('../lib/httpwsbridge');
 //connect with browser
 route.ws('/web', (ws, req) => {
-
+  console.log(req.url);
   // console.log("ws from web req=" + JSON.stringify(req));
-  wsWeb = ws;
-  httpwsbridge.updateWebClient(ws);
+  httpwsbridge.addWebClient(ws);
   ws.on('message', function (msg) {
     console.log(msg);
     // console.log(msg.command);
     // ws.send(msg);
     var obj = JSON.parse(msg);
+    // console.log(obj);
     console.log(obj.command);
 
     if (obj) {
       // console.log('parse and send')
-      if (wsMobile) {
-        wsMobile.send(obj);
-        console.log('trasfer msg to mobile msg=' + msg);
-      }
+      httpwsbridge.sendMsgToMobileClients(msg);
+      console.log('trasfer msg to mobile msg=' + msg);
     }
   })
 
   
   ws.on('close', function (e) {
-      wsWeb = null;
+      httpwsbridge.removeWebClient(ws);
   })
 })
 
 
 // connect with esp32
-route.ws('/mobile', (ws, req) => {
-  wsMobile = ws;
-  httpwsbridge.updateMobileClient(ws);
-  console.log('mobile connected')
-  wsMobile.send("hello esp32")
+route.ws('/mobile/hub', (ws, req) => {
+  console.log(req.url);
+  httpwsbridge.addMobileClient(ws);
   ws.on('message', function (msg) {
     console.log(msg);
     
@@ -50,8 +42,38 @@ route.ws('/mobile', (ws, req) => {
 
   
   ws.on('close', function (e) {
-      wsMobile = null;
+      httpwsbridge.removeMobileClient(ws);
   })
 })
 
+
+// connect with camera
+route.ws('/mobile/camera/control',  (ws, req) => {
+  console.log(req.url);
+  httpwsbridge.addMobileClient(ws);
+  ws.on('message', function (msg) {
+    console.log(msg);
+    
+  })
+
+  
+  ws.on('close', function (e) {
+      httpwsbridge.removeMobileClient(ws);
+  })
+})
+
+// connect with camera
+route.ws('/mobile/camera/stream',  (ws, req) => {
+  console.log(req.url);
+  // httpwsbridge.addMobileClient(ws);
+  ws.on('message', function (msg) {
+    console.log(msg.length);
+    
+  })
+
+  
+  ws.on('close', function (e) {
+      // httpwsbridge.removeMobileClient(ws);
+  })
+})
 module.exports = route 
